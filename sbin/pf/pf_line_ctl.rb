5ce09a0d3f56cf9fa00fda0ba9d07f6322a339ff
robertburrowes <r.burrowes@auckland.ac.nz>
2022-07-24 16:21:59 +1200

    copied running version on top of git one.
    Some style differences.
    Git version missing WIKK:: line 14

--- sbin/pf/pf_line_ctl.rb
+++ sbin/pf/pf_line_ctl.rb
@@ -11,76 +11,77 @@ require_relative "#{RLIB}/wikk_conf.rb"
 @num_lines = @line_ctl.line.length - 1 # number of defined, but not necessarily active, external lines.
 
 # Which line a host uses (by default) is defined here
-@host_line_map = Configuration.new("#{PF_CONF_DIR}/host_line_map.json")
+@host_line_map = WIKK::Configuration.new("#{PF_CONF_DIR}/host_line_map.json")
 
 # Check that the xDSL modems are up, and connected to the ISP
 adsl_pingable = []  # Can ping xDSL modem
 line_active = []    # Can ping the external IP of the xDSL modem, so we know it should be connected to ISP
 
 (1..@num_lines).each do |i| # Skip 0, as we start numbering dsl modems at 1, while the array numbers from 0
-  begin
-    # Check if xDSL line is administratively up, though it may still not be connected to ISP.
-    if @line_ctl.line[i]['active']
-      # See if the modem is actually working. Internal network test
-      system("#{FPING} -u #{@line_ctl.line[i]['hostname']}")
-      adsl_return_code = $CHILD_STATUS
-      adsl_pingable[i] = adsl_return_code.to_i == 0
-      if adsl_pingable[i]
-        # If line has a config script, run it.
-        if ROLE == 'PRIMARY_PF' && @line_ctl.line[i]['config_script'] != nil
-          puts "Running #{SBIN_DIR}/pf/#{@line_ctl.line[i]['config_script']}"
-          system("#{SBIN_DIR}/pf/#{@line_ctl.line[i]['config_script']}")
-        end
-
-        # see if line is connected to the ISP. Have to know external IP address.
-        system("#{FPING} -u external#{i}")
-        ex_return_code = $CHILD_STATUS
-        line_active[i] = ex_return_code.to_i == 0 # Line is active, as internal and external ping worked.
-      else # Can't ping the xDSL router, so we wouldn't be able to ping the external line.
-        puts "Adsl modem down line[#{i}] hostname=#{@line_ctl.line[i]['hostname']}"
-        line_active[i] = false # Line is inactive, as internal ping failed.
+  # Check if xDSL line is administratively up, though it may still not be connected to ISP.
+  if @line_ctl.line[i]['active']
+    # See if the modem is actually working. Internal network test
+    system("#{FPING} -u #{@line_ctl.line[i]['hostname']}")
+    adsl_return_code = $CHILD_STATUS
+    adsl_pingable[i] = adsl_return_code.to_i == 0
+    puts "#{@line_ctl.line[i]['hostname']} is active, state is administratively #{@line_ctl.line[i]['up'] ? 'up' : 'down'}, Pingable #{adsl_pingable[i]}"
+    if adsl_pingable[i] && @line_ctl.line[i]['up']
+      # If line has a config script, run it.
+      if ROLE == 'PRIMARY_PF' && !@line_ctl.line[i]['config_script'].nil?
+        puts "Running #{SBIN_DIR}/pf/#{@line_ctl.line[i]['config_script']}"
+        system("#{SBIN_DIR}/pf/#{@line_ctl.line[i]['config_script']}")
       end
-    else # Line is administratively offline. (Don't bother pinging it)
-      adsl_pingable[i] = line_active[i] = false
+
+      # see if line is connected to the ISP. Have to know external IP address.
+      system("#{FPING} -u external#{i}")
+      ex_return_code = $CHILD_STATUS
+      line_active[i] = ex_return_code.to_i == 0 # Line is active, as internal and external ping worked.
+    else # Can't ping the xDSL router, so we wouldn't be able to ping the external line.
+      line_active[i] = false # Line is inactive, as internal ping failed.
     end
-  rescue StandardError => e
-    puts "Error for line[#{i}]: #{e}"
+  else # Line is administratively offline. (Don't bother pinging it)
     adsl_pingable[i] = line_active[i] = false
   end
+rescue StandardError => e
+  puts "Error for line[#{i}]: #{e}"
+  adsl_pingable[i] = line_active[i] = false
 end
 
 active_lines = [] # These are lines that are up
 failure_map = [] # Map of line number allocated to sites, to active line number
 # Build failure mapping from inactive to active lines.
 (1..@num_lines).each do |i|
+  failure_map[i] = []
   if line_active[i]
-    failure_map[i] = i # i.e. maps to itself, so we don't have to test later.
+    failure_map[i] << i # i.e. maps to itself, so we don't have to test later.
     active_lines << i # Note that this line is up.
-  elsif @line_ctl.line[i]['failure_map'] != nil # Line is inactive
-    @line_ctl.line[i]['failure_map'].each do |m|
-      if line_active[m] # first active line in map replaces this one
-        failure_map[i] = m
-        break
+  else # Line is inactive
+    unless @line_ctl.line[i]['failure_map'].nil? # But has a fail over line defined.
+      @line_ctl.line[i]['failure_map'].each do |m|
+        failure_map[i] << m if line_active[m] # active lines in map replaces this one
       end
-    end # But has a fail over line defined.
+    end
   end
 end
 
 out = [] # This is the output queue.
 (0..@num_lines).each { |i| out[i] = [] } # Base assumptions is all lines have array of clients, even if array empty.
 
-missed = []    # These are site entries for lines that are down, hence need to be load balanced over other lines
+missed = [] # These are site entries for lines that are down, hence need to be load balanced over other lines
 
 # Assign users to lines, testing for line being up, and explicit mappings of entire lines to other lines
 (1..@num_lines).each do |i|
-  if @host_line_map.line[i] != nil # LINE Has sites assigned to it (even if the line is inactive)
-    if failure_map[i].nil? # A line is down, so assign the sites on this line to the missed queue
+  unless @host_line_map.line[i].nil? # LINE Has sites assigned to it (even if the line is inactive)
+    if !failure_map[i].nil? && failure_map[i].length > 0 # The line (or its alternate) is active
+      index = 0
+      @host_line_map.line[i].each do |host, network|
+        target_line = index % failure_map[i].length
+        out[failure_map[i][target_line]] << [ host, network, i ] # Populate out queue with host, network and what the line should have been
+        index += 1
+      end
+    else # A line is down, so assign the sites on this line to the missed queue
       @host_line_map.line[i].each do |host, network|
         missed << [ host, network, i ] # Note the sites on lines that are down, and have no defined alternate line.
-      end
-    else # The line (or its alternate) is active
-      @host_line_map.line[i].each do |host, network|
-        out[failure_map[i]] << [ host, network, i ] # Populate out queue with host, network and what the line should have been
       end
     end
   end
@@ -103,7 +104,7 @@ if active_lines.length > 0 # at least one line is up ;)
         if i == o[2] # o[2] is the line this entry should have been on.
           fd.puts "#{o[0]}\t#{o[1]}\tblack" # o[0] is the site name. o[1] is the ip/maskbits, Black: site on prefered line.
         else
-          fd.puts "#{o[0]}\t#{o[1]}\tred"    # o[0] is the site name. o[1] is the ip/maskbits. Red: site on alternate line.
+          fd.puts "#{o[0]}\t#{o[1]}\tred" # o[0] is the site name. o[1] is the ip/maskbits. Red: site on alternate line.
         end
       end
     end
@@ -114,13 +115,19 @@ else
   # they will do so together, and it is best to have pf tables ready.
   (1..@num_lines).each do |i|
     File.open("#{PF_WRK_DIR}/table_line_#{i}", 'w') do |fd|
-      if @host_line_map.line[i] != nil
-        @host_line_map.line[i].each { |_host, network| fd.puts "   #{network}" } # Line per site, outputing the site ip/maskbits for pf to load into tables
+      unless @host_line_map.line[i].nil?
+        # Line per site, outputing the site ip/maskbits for pf to load into tables
+        @host_line_map.line[i].each do |_host, network|
+          fd.puts "   #{network}"
+        end
       end
     end
     File.open("#{PF_WRK_DIR}/table_line_state_#{i}", 'w') do |fd|
-      if @host_line_map.line[i] != nil
-        @host_line_map.line[i].each { |host, network| fd.puts "#{host}\t#{network}\tblack" } # Site, ip/maskbits, black for on correct line
+      unless @host_line_map.line[i].nil?
+        # Site, ip/maskbits, black for on correct line
+        @host_line_map.line[i].each do |host, network|
+          fd.puts "#{host}\t#{network}\tblack"
+        end
       end
     end
   end
