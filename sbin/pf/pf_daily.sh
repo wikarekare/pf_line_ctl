2e3d1ce3fcfa519db58adbfb61fe543f944555da
robertburrowes <r.burrowes@auckland.ac.nz>
2023-03-30 18:36:52 +1300

    wrong key in script

--- sbin/pf/pf_daily.sh
+++ sbin/pf/pf_daily.sh
@@ -6,6 +6,6 @@ ${LOCKFILE} ${LOCK_PID_FILE} $$
 if [ $? != 0 ] ; then  exit 0 ; fi
 
 ${SBIN_DIR}/pf/pf_host_line_map_update.rb
-scp -i ${SSH_KEY_DIR}/wikk_id_rsa ${PF_CONF_DIR}/host_line_map.json ${LINE}@${PF_HOST}:${PF_CONF_DIR_REMOTE}/host_line_map.json
+scp -i ${SSH_KEY_DIR}/line_id_rsa ${PF_CONF_DIR}/host_line_map.json ${LINE}@${PF_HOST}:${PF_CONF_DIR_REMOTE}/host_line_map.json
 
 rm -f ${LOCK_PID_FILE}
