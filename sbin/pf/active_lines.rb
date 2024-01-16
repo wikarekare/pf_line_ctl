14a83e4dde106518946aa23a706a7c2f3f39adc3
robertburrowes <r.burrowes@auckland.ac.nz>
2024-01-16 14:44:50 +1300

    Introduced two util calls to remove hard coded line list in script. Copy line_active.json to web server

---
+++ sbin/pf/active_lines.rb
@@ -0,0 +1,15 @@
+#!/usr/local/bin/ruby
+require 'json'
+require 'wikk_configuration'
+require 'wikk_json'
+RLIB = '/wikk/rlib' unless defined? RLIB
+require_relative "#{RLIB}/wikk_conf.rb"
+
+@line_ctl = WIKK::Configuration.new("#{PF_CONF_DIR}/line_state.json")
+@num_lines = @line_ctl.line.length - 1
+
+lines = []
+(1..@num_lines).each do |i| # Skip 0, as we start numbering dsl modems at 1, while the array numbers from 0
+  lines << i if @line_ctl.line[i]['active']
+end
+puts lines.join(' ')
--- sbin/pf/pf_table_update.sh
+++ sbin/pf/pf_table_update.sh
@@ -13,10 +13,13 @@ LOCK_PID_FILE=${TMP_DIR}/pf/pf_cron.lock
 ${LOCKFILE} ${LOCK_PID_FILE} $$
 if [ $? != 0 ] ; then  exit 0 ; fi
 
+active_lines=$(/wikk/sbin/pf/active_lines.rb)
+all_lines=$(/wikk/sbin/pf/all_lines.rb)
+
 if [ -e /dev/pf ]
 then
 
-    for i in 5 6 7; do
+    for i in $all_lines ; do
       if [ ! -e ${PF_WRK_DIR}/table_line_${i} ]
       then
         ${CP} /dev/null ${PF_WRK_DIR}/table_line_${i}
@@ -27,19 +30,20 @@ then
       fi
     done
 
-    for i in 5 6 7; do
+    for i in $all_lines ; do
       ${PFCTL} -t local_nets_${i} -T show | ${SORT} > ${PF_WRK_DIR}/local_nets_${i}
     done
 
+    # This checks the line status to see what is actually up.
     ${SBIN_DIR}/pf/pf_line_ctl.rb
 
-    for i in 5 6 7; do
+    for i in $active_lines ; do
      ${SORT} -u ${PF_WRK_DIR}/table_line_${i} > ${PF_WRK_DIR}/table_line_${i}.srt
     done
 
     #decommissioned lines 1 to 3
     #So only need to do this for lines 5 through 7
-    for i in 5 6 7; do
+    for i in $active_lines ; do
       ${CMP} -s ${PF_WRK_DIR}/local_nets_${i} ${PF_WRK_DIR}/table_line_${i}.srt
       if [ $? != 0 ]
       then
@@ -53,14 +57,15 @@ then
         #Make copies on the web server, so we can easily access the current status.
         #Should be derived from DB, so web server would just do lookup
         echo "Copying line state files to web server"
-        for i in 5 6 7; do
+        ${SCP} -i /home/line/.ssh/id_rsa  ${PF_WRK_DIR}/line_active.json ${LINE}@${WWW_SRV}:line/line_active.json
+        for i in $active_lines ; do
           ${SCP} -i /home/line/.ssh/id_rsa  ${PF_WRK_DIR}/table_line_${i}.srt ${LINE}@${WWW_SRV}:line/line${i}.txt
           ${SCP} -i /home/line/.ssh/id_rsa  ${PF_WRK_DIR}/table_line_state_${i} ${LINE}@${WWW_SRV}:line/line${i}_state.txt
         done
     else
             #Make copies on the web server, so we can easily access the current status.
         #Should be derived from DB, so web server would just do lookup
-        for i in 5 6 7; do
+        for i in $active_lines ; do
           ${CP} ${PF_WRK_DIR}/table_line_${i}.srt ${LINE_WWW_DIR}/line${i}.txt
           ${CP} ${PF_WRK_DIR}/table_line_state_${i} ${LINE_WWW_DIR}/line${i}_state.txt
         done
