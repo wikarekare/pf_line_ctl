41d9c1c9ef1917cb26f60e0545cfc44ad16f4c16
robertburrowes <r.burrowes@auckland.ac.nz>
2024-01-16 14:47:05 +1300

    Should have been copying to the standby gate too.

--- sbin/pf/pf_daily.sh
+++ sbin/pf/pf_daily.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 . /wikk/etc/wikk.conf
+SCP=/usr/bin/scp
 
 LOCK_PID_FILE=${TMP_DIR}/pf_daily.lock
 ${LOCKFILE} ${LOCK_PID_FILE} $$
 if [ $? != 0 ] ; then  exit 0 ; fi
 
 ${SBIN_DIR}/pf/pf_host_line_map_update.rb
-scp -i ${SSH_KEY_DIR}/line_id_rsa ${PF_CONF_DIR}/host_line_map.json ${LINE}@${PF_HOST}:${PF_CONF_DIR_REMOTE}/host_line_map.json
+${SCP} -i ${SSH_KEY_DIR}/line_id_rsa ${PF_CONF_DIR}/host_line_map.json ${LINE}@${PF_HOST_1}:${PF_CONF_DIR_REMOTE}/host_line_map.json
+${SCP} -i ${SSH_KEY_DIR}/line_id_rsa ${PF_CONF_DIR}/host_line_map.json ${LINE}@${PF_HOST_2}:${PF_CONF_DIR_REMOTE}/host_line_map.json
 
-rm -f ${LOCK_PID_FILE}
+${RM} -f ${LOCK_PID_FILE}
