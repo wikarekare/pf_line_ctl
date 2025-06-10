9a7dfbc8885cc9260c22c6785675775c478e2edd
robertburrowes <r.burrowes@auckland.ac.nz>
2025-06-10 17:36:11 +1200

    Tmp commenting out of 1 server. Will look at automating this.

--- sbin/pf/pf_daily.sh
+++ sbin/pf/pf_daily.sh
@@ -7,7 +7,8 @@ ${LOCKFILE} ${LOCK_PID_FILE} $$
 if [ $? != 0 ] ; then  exit 0 ; fi
 
 ${SBIN_DIR}/pf/pf_host_line_map_update.rb
-${SCP} -i ${SSH_KEY_DIR}/line_id_rsa ${PF_CONF_DIR}/host_line_map.json ${LINE}@${PF_HOST_1}:${PF_CONF_DIR_REMOTE}/host_line_map.json
+# While other gate is inactive, comment out the sync. Should check this dynamically!
+# ${SCP} -i ${SSH_KEY_DIR}/line_id_rsa ${PF_CONF_DIR}/host_line_map.json ${LINE}@${PF_HOST_1}:${PF_CONF_DIR_REMOTE}/host_line_map.json
 ${SCP} -i ${SSH_KEY_DIR}/line_id_rsa ${PF_CONF_DIR}/host_line_map.json ${LINE}@${PF_HOST_2}:${PF_CONF_DIR_REMOTE}/host_line_map.json
 
 ${RM} -f ${LOCK_PID_FILE}
